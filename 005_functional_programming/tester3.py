423dc9a049744dd5d29bc1ea1f388366bc2f3eac
Roman Kutselepa <roman.kutselepa@gmail.com>
2023-07-12 20:11:17 +0300

    datetime

--- 005_functional_programming/tester3.py
+++ 005_functional_programming/tester3.py
@@ -18,11 +18,11 @@
 # sample()
 # sample()
 
-def sample2(a, b, c=1, *args, **kwargs):
-    print(a * b * c)
-    for arg in args:
-        print(arg)
-    print(kwargs)
-
-
-sample2(12, 12, 3, 'hello', 1, 'asdasd', 123, 0.32, [1, 2, 3], name='Jack', x=10, surname='Smith')
+# def sample2(a, b, c=1, *args, **kwargs):
+#     print(a * b * c)
+#     for arg in args:
+#         print(arg)
+#     print(kwargs)
+#
+#
+# sample2(12, 12, 3, 'hello', 1, 'asdasd', 123, 0.32, [1, 2, 3], name='Jack', x=10, surname='Smith')
---
+++ 006_calendar_time_datetime/tester.py
@@ -0,0 +1,7 @@
+import datetime
+
+today = datetime.datetime.today()
+
+
+print(today.timestamp())
+print(datetime.datetime.fromtimestamp(1689181771.699912))
